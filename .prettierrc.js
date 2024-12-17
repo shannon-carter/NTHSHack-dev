2c9d4b60030e9258ac9f46117dde14664ee884f8
shannon-carter <shannon.carter@acmutd.co>
2024-12-17 14:42:26 -0600

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  overrides: [
+    {
+      files: '*.svg',
+      options: {
+        parser: 'html',
+      },
+    },
+  ],
+};
